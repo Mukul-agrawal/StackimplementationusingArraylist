c3c98ede60c28f6a39526ea83e679be66b23ebdd
Mukul-agrawal <mukul.agrawal_cs18@gla.ac.in>
2020-05-14 19:01:22 +0530

    create menu

--- src/execution/Main.java
+++ src/execution/Main.java
@@ -1,7 +1,37 @@
 package execution;
 
+import defination.MyStack;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+        MyStack<Integer> stack = new MyStack<>();
+        final Scanner scanner = new Scanner(System.in);
+        int choice = 0;
+        while (true) {
+            System.out.println("Press 1 to \"Push\" a node at the top.");
+            System.out.println("Press 2 to \"Pop\" a node from the top.");
+            System.out.println("Press 3 to \"Peek\" at the node at the top.");
+            System.out.println("Press 4 to exit.");
+            choice = scanner.nextInt();
+            switch (choice) {
+                case 1:
+                    System.out.println("Enter the data(int num) of the node to push: ");
+                    int data = scanner.nextInt();
+                    stack.push(data);
+                    break;
+                case 2:
+                    int poppedData = stack.pop();
+                    break;
+                case 3:
+                    System.out.println("Top of the node: " + stack.peek());
+                    break;
+                case 4:
+                    System.exit(0);
+                default:
+                    System.out.println("Please enter a valid choice!");
+            }
+        }
     }
 }

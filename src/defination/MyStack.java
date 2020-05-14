abb1dd58cd815b75fc486e199ea5b31da4b645cd
Mukul-agrawal <mukul.agrawal_cs18@gla.ac.in>
2020-05-14 18:48:40 +0530

    implements the StackADT interface and override the methods

--- src/defination/MyStack.java
+++ src/defination/MyStack.java
@@ -1,4 +1,25 @@
 package defination;
 
-public class MyStack<E> {
+import adt.StackADT;
+
+public class MyStack<E> implements StackADT<E> {
+    @Override
+    public E push(E obj) {
+        return null;
+    }
+
+    @Override
+    public E peek() {
+        return null;
+    }
+
+    @Override
+    public E pop() {
+        return null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
 }
